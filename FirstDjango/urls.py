0d4bdb5d9edacd0157afb19c662204b5ec216c29
IvanRevin <revin.is@yandex.ru>
2024-01-17 20:04:16 +0300

    Add item model and make migrt

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,28 +1,10 @@
-"""
-URL configuration for FirstDjango project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.text),
-    path('about/', views.about),
-    path('about1/', views.about1),  
-    path('item/<int:item_id>/', views.get_item),
-    path('items/', views.items_),
-    
+    path('', views.home),
+    path('about', views.about),
+    path('item/<int:item_id>', views.get_item),
+    path('items', views.get_items),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,63 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+
+# Create your views here.
 author = {
-      "Имя     ": "Иван",
-      "Отчество": "Петрович",
-      "Фамилия ": "Иванов",
-      "телефон ": "8-923-600-01-02",
-      "email   ": "vasya@mail.ru",
+       "Имя": "Иван",
+       "Отчество": "Петрович",
+       "Фамилия": "Иванов",
+       "телефон": "8-923-600-01-02",
+       "email": "vasya@mail.ru",
 }
 
 items = [
-   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-   {"id": 7, "name": "Картофель фри" ,"quantity":0},
-   {"id": 8, "name": "Кепка" ,"quantity":124},
+   {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+   {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+   {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
+   {"id": 7, "name": "Картофель фри", "quantity": 0},
+   {"id": 8, "name": "Кепка", "quantity": 124},
 ]
 
 
-def text(requst):
-    return render(requst,'index.html')
-    
+def home(request):
+    context = {
+        "name": "Петров Иван Николаевич",
+        "email": "my_mail@mail.ru"
+    }
+    return render(request, "index.html", context)
+
 
 def about(request):
-    return HttpResponse(
-        """<p>Имя: <strong>Иван</strong></p>
-<p>Отчество: <strong>Петрович</strong></p>
-<p>Фамилия: <strong>Иванов</strong></p>
-<p>телефон: <strong>8-923-600-01-02</strong></p>
-<p>email: <strong>vasya@mail.ru</strong></p>
-"""
-    )
-
-def about1(request):
-    text =f"""Имя: <b>{author['Имя']}</b><br>
-<p>Отчество: <b>{author['Отчество']}</b><br>
-<p>Фамилия: <b>{author['Фамилия']}</b><br>
-<p>телефон: <b>{author['телефон']}</b><br>
-<p>email:   <b>{author['email']}</b><br>
-"""
+    text = f"""
+       <header>
+       / <a href="/"> Home </a> / <a href="/items"> Items </a> / <a href="/about"> About </a>
+       </header><br>
+       Имя: <b>{author['Имя']}</b><br>
+       Отчество: <b>{author['Отчество']}</b><br>
+       Фамилия: <b>{author['Фамилия']}</b><br>
+       телефон: <b>{author['телефон']}</b><br>
+       email: <b>{author['email']}</b><br>
+       """
     return HttpResponse(text)
 
+
 def get_item(request, item_id: int):
-    """по указанному id возвращаем товар"""
+    """ По указанному item_id возращаем имя элемента и количество. """
     for item in items:
-        if item['id']==item_id:
-            item_name = F"""
-            <p>name: <b>{item['name']}</b><br>
-            <p>quantity: <b>{item['quantity']}</b><br>"""
-            return render(request,'itemlist.html',context)
-    return HttpResponseNotFound(f"not found {item_id}")
+        if item['id'] == item_id:
+            context = {
+                "item": item
+            }
+            return render(request, "item_page.html", context)
+    return HttpResponseNotFound(f'Item with id={item_id} not found')
 
 
-def items_(request):
-    items_li = '<ol>'
-    for item in items:
-        items_li += f"""<li><a href="/item/{item['id']}">Имя: {item['name']}</a></li>"""
-    items_li += '</ol>'
-    return HttpResponse(items_li)
-
-                    
-    
+def get_items(request):
+    context = {
+        "items": items
+    }
+    return render(request, "items_list.html", context)
